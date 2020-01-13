93451b4d46c99e8820a7f07eceb7e12d483937f9
caixw <xnotepad@gmail.com>
2020-01-13 23:27:37 +0800

    feat: 添加基本的语法高亮功能

---
+++ src/languages/source.py
@@ -0,0 +1,46 @@
+
+
+"""
+ * 文档内容
+ * <undefined>undefined</undefined>
+ * <apidoc version="1.0.0">
+ *    <title>xxx</title>
+ *
+ *    <api version="1.0.0">
+ *        <title>xxx</title>
+ *    </api>  
+ *    <api version="1.0.0">
+ *        <title>xxx</title>
+ *    </api>
+ * </apidoc>
+"""
+
+# 文档内容
+# <apidoc version="1.0.0">
+#    <title>xxx</title>
+#
+#    <api version="1.0.0">
+#        <title>xxx</title>
+#    </api>  
+#    <api version="1.0.0">
+#        <title>xxx</title>
+#    </api>
+# </apidoc>
+
+
+'''
+ <api version="1.0.0">
+     <title>xxx</title>
+ </api>  
+'''
+
+
+# <api version="1.0.0">
+#     <title>xxx</title>
+# </api>  
+
+
+# <api version="1.0.0" deprecated="1.0.0"
+# method="GET">
+#     <title>xxx</title>
+# </api>

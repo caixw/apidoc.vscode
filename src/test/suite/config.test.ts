3837b5422818944fcf7ad06bc6f815ee4e0f9fa7
caixw <xnotepad@gmail.com>
2020-07-06 20:20:31 +0800

    test: 添加针对 config 的测试

--- src/test/suite/config.test.ts
+++ src/test/suite/config.test.ts
@@ -1,3 +1,23 @@
 // SPDX-License-Identifier: MIT
 
-// TODO
+import * as assert from 'assert';
+import * as config from '@/config';
+
+interface Selector {
+    language: string;
+    scheme: string;
+}
+
+suite('config test suite', () => {
+    test('document selector', () => {
+        const selector = config.documentSelector();
+
+        assert.ok(selector.length > 0)
+
+        for (const item of selector) {
+            const s = <Selector>item;
+            assert.equal(s.scheme, 'file');
+            assert.ok(s.language.length > 0);
+        }
+    });
+});

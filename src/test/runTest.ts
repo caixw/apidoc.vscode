5e2c2e0761411fcebb978e5e4b80c3114726153e
caixw <xnotepad@gmail.com>
2020-07-06 20:13:02 +0800

    build: 修正各类构建错误

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,23 +1,27 @@
-import * as path from 'path';
+// SPDX-License-Identifier: MIT
 
+import * as path from 'path';
 import { runTests } from 'vscode-test';
+
+// 项目的根目录。即 package.json 所在的目录
+//
+// 同时作为 extensionDevelopmentPath 参数使用；
+// 同时还会将该目录的内容加载到启动的 vscode 测试用例中。
+const rootDir = path.resolve(__dirname, '../../');
+
+const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({ extensionDevelopmentPath: rootDir, extensionTestsPath });
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
 }
 
-main();
+main().catch((reason) => {
+    console.error(reason);
+    process.exit(1);
+});
